f3cdad69db564494ccb111f5b6d4d8b786ecde19
rhythmraheja <rahejarhythm29@gmail.com>
2025-03-19 13:30:34 +0530

    fixed mysql module not found

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from routes import resume_routes, job_routes
 from config import Config
 from models import db
-
+import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend-backend communication
 
@@ -14,5 +14,8 @@ db.init_app(app)
 app.register_blueprint(resume_routes)
 app.register_blueprint(job_routes)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
